d518ef635d84941196f383884900f5d0b0db5c32
Dimusik52 <volodya.pikhulya@mail.ru>
2023-08-04 19:48:04 +0300

    Добавлено поле "Дата изменения" и связанные действия в этом поле, указанные в задании 2

--- advertisements/app_advertisements/models.py
+++ advertisements/app_advertisements/models.py
@@ -25,3 +25,13 @@ class Advertisement(models.Model):
             created_time = self.created_at.time().strftime("%H:%M:%S")
             return format_html('<span style ="color: green; font-weight: bold;">Сегодня в {}</span>', created_time)
         return self.created_at.strftime("%d.%m.%Y в %H:%M:%S")
+    
+    @admin.display(description="Дата изменения")
+    def updated_date(self):
+        from django.utils import timezone
+        from django.utils.html import format_html
+        if self.updated_at.date() == timezone.now().date():
+            updated_time = self.updated_at.time().strftime("%H:%M:%S")
+            return format_html('<span style ="color: yellow; font-weight: bold;">Сегодня в {}</span>', updated_time)
+        return self.updated_at.strftime("%d.%m.%Y в %H:%M:%S")
+    
